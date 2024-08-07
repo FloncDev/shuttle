c9401f089340c77e170d8769e0c47c333efdcc6a
FloncDev <FloncDev@users.noreply.github.com>
2024-08-07 22:20:07 +0200

    ✨ Implement get endpoints for duo games

--- api/src/models/game.rs
+++ api/src/models/game.rs
@@ -22,14 +22,72 @@ pub struct Team {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Set(i32, i32);
 
-#[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct DuoGame {
     pub id: i32,
     pub created_at: DateTime<Utc>,
     pub first_to: i32,
     pub team_one: Team,
     pub team_two: Team,
-    pub sets: Vec<[i32; 2]>,
+    pub sets: Json<Vec<[i32; 2]>>,
+}
+
+impl<'r> FromRow<'r, PgRow> for DuoGame {
+    fn from_row(row: &'r PgRow) -> Result<Self, sqlx::Error> {
+        let id = row.try_get("id")?;
+        let created_at = row.try_get("created_at")?;
+        let first_to = row.try_get("first_to")?;
+        let sets = row.try_get("sets")?;
+
+        let t1p1 = Player {
+            id: row.try_get("t1p1_id")?,
+            name: row.try_get("t1p1_name")?,
+            rating: row.try_get("t1p1_rating")?,
+            uncertainty: row.try_get("t1p1_uncertainty")?,
+        };
+
+        let t1p2 = Player {
+            id: row.try_get("t1p2_id")?,
+            name: row.try_get("t1p2_name")?,
+            rating: row.try_get("t1p2_rating")?,
+            uncertainty: row.try_get("t1p2_uncertainty")?,
+        };
+
+        let t2p1 = Player {
+            id: row.try_get("t2p1_id")?,
+            name: row.try_get("t2p1_name")?,
+            rating: row.try_get("t2p1_rating")?,
+            uncertainty: row.try_get("t2p1_uncertainty")?,
+        };
+
+        let t2p2 = Player {
+            id: row.try_get("t2p2_id")?,
+            name: row.try_get("t2p2_name")?,
+            rating: row.try_get("t2p2_rating")?,
+            uncertainty: row.try_get("t2p2_uncertainty")?,
+        };
+
+        let team_one = Team {
+            id: row.try_get("t1_id")?,
+            player_one: t1p1,
+            player_two: t1p2,
+        };
+
+        let team_two = Team {
+            id: row.try_get("t2_id")?,
+            player_one: t2p1,
+            player_two: t2p2,
+        };
+
+        Ok(DuoGame {
+            id,
+            created_at,
+            first_to,
+            team_one,
+            team_two,
+            sets,
+        })
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
--- api/src/routers/duo_game.rs
+++ api/src/routers/duo_game.rs
@@ -7,22 +7,34 @@ use axum::{
 use crate::{AppState, DuoGame, Result, User};
 
 async fn get_games(State(state): State<AppState>) -> Result<Json<Vec<DuoGame>>> {
-    let games_db = sqlx::query!(
-        r#"
+    Ok(Json(
+        sqlx::query_as(
+            r#"
             SELECT
             duo_games.id, duo_games.created_at, duo_games.first_to,
 
-            t1.id as team_one_id,
-            t1p1.id as team_one_player_one_id,
-            t1p1.name as team_one_player_one_name,
-            t1p2.id as team_one_pl1ayer_two_id,
-            t1p2.name as team_one_player_two_name,
+            t1.id as t1_id,
+            t1p1.id as t1p1_id,
+            t1p1.name as t1p1_name,
+            t1p1.rating as t1p1_rating,
+            t1p1.uncertainty as t1p1_uncertainty,
 
-            t2.id as team_two_id,
-            t2p1.id as team_two_player_one_id,
-            t2p1.name as team_two_player_one_name,
-            t2p2.id as team_two_pl1ayer_two_id,
-            t2p2.name as team_two_player_two_name
+            t1p2.id as t1p2_id,
+            t1p2.name as t1p2_name,
+            t1p2.rating as t1p2_rating,
+            t1p2.uncertainty as t1p2_uncertainty,
+
+            t2.id as t2_id,
+            t2p1.id as t2p1_id,
+            t2p1.name as t2p1_name,
+            t2p1.rating as t2p1_rating,
+            t2p1.uncertainty as t2p1_uncertainty,
+
+            t2p2.id as t2p2_id,
+            t2p2.name as t2p2_name,
+            t2p2.rating as t2p2_rating,
+            t2p2.uncertainty as t2p2_uncertainty
+
             FROM duo_games
             JOIN teams t1 ON t1.id = duo_games.team_one
             JOIN users t1p1 ON t1.player_one = t1p1.id
@@ -31,16 +43,61 @@ async fn get_games(State(state): State<AppState>) -> Result<Json<Vec<DuoGame>>> 
             JOIN teams t2 ON t2.id = duo_games.team_two
             JOIN users t2p1 ON t2.player_one = t2p1.id
             LEFT JOIN users t2p2 ON t2.player_two = t2p2.id
-        "#
-    )
-    .fetch_all(&state.pool)
-    .await?;
-
-    todo!();
+        "#,
+        )
+        .fetch_all(&state.pool)
+        .await?,
+    ))
 }
 
-async fn get_game(State(state): State<AppState>, _: User, Path(id): Path<i32>) -> Json<DuoGame> {
-    todo!();
+async fn get_game(
+    State(state): State<AppState>,
+    _: User,
+    Path(id): Path<i32>,
+) -> Result<Json<DuoGame>> {
+    Ok(Json(
+        sqlx::query_as(
+            r#"
+            SELECT
+            duo_games.id, duo_games.created_at, duo_games.first_to,
+
+            t1.id as t1_id,
+            t1p1.id as t1p1_id,
+            t1p1.name as t1p1_name,
+            t1p1.rating as t1p1_rating,
+            t1p1.uncertainty as t1p1_uncertainty,
+
+            t1p2.id as t1p2_id,
+            t1p2.name as t1p2_name,
+            t1p2.rating as t1p2_rating,
+            t1p2.uncertainty as t1p2_uncertainty,
+
+            t2.id as t2_id,
+            t2p1.id as t2p1_id,
+            t2p1.name as t2p1_name,
+            t2p1.rating as t2p1_rating,
+            t2p1.uncertainty as t2p1_uncertainty,
+
+            t2p2.id as t2p2_id,
+            t2p2.name as t2p2_name,
+            t2p2.rating as t2p2_rating,
+            t2p2.uncertainty as t2p2_uncertainty
+
+            FROM duo_games
+            JOIN teams t1 ON t1.id = duo_games.team_one
+            JOIN users t1p1 ON t1.player_one = t1p1.id
+            LEFT JOIN users t1p2 ON t1.player_two = t1p2.id
+
+            JOIN teams t2 ON t2.id = duo_games.team_two
+            JOIN users t2p1 ON t2.player_one = t2p1.id
+            LEFT JOIN users t2p2 ON t2.player_two = t2p2.id
+            WHERE duo_game.id = $1
+        "#,
+        )
+        .bind(id)
+        .fetch_one(&state.pool)
+        .await?,
+    ))
 }
 
 async fn post_game(State(state): State<AppState>, _: User, game: Json<DuoGame>) -> Json<DuoGame> {
